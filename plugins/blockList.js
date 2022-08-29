fa19533c8f9377cfe5750f20569594100de9ccd6
nanaiq <108739585+nanaiq@users.noreply.github.com>
2022-08-29 18:14:00 +0700

    Update blockList.js

--- plugins/blockList.js
+++ plugins/blockList.js
@@ -7,6 +7,6 @@ conn.reply(m.chat, 'List Block:\n\n' + `Total: ${block == undefined ? '*0* Diblo
 handler.help = ['blocklist']
 handler.tags = ['owner']
 handler.command = /^listbloc?k|bloc?klist|daftarbloc?k|blocks$/i
-handler.owner = false
+handler.owner = true
 
 module.exports = handler
